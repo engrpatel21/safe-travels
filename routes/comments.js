384327968e5057906674f608dce7fcb33a303fff
kkonkel23 <kailahkonkel@gmail.com>
2020-10-01 19:44:27 -0400

    add delete button and tips page

--- routes/comments.js
+++ routes/comments.js
@@ -3,6 +3,7 @@ const commentsCtrl = require('../controllers/comments')
 
 router.get('/', commentsCtrl.index)
 router.post('/', isLoggedIn, commentsCtrl.create)
+router.delete('/:id', isLoggedIn, commentsCtrl.delete)
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
