d070f445410e773e52dcf9b07c7c08efab289d50
pratik <engrpatel21@gmail.com>
2020-10-02 12:37:04 -0400

    anotherp ush

--- controllers/api.js
+++ controllers/api.js
@@ -9,16 +9,20 @@ module.exports = {
 function address(req, res){
     console.log(req.body)
     axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.body.streetNumber}+${req.body.streetName},+${req.body.state},+${req.body.state}&key=AIzaSyC7XewzzlIpPEnLlh_MOpUoaWoipdyBM_8`)
-    .then(response =>{
-        let county = response.data.results[0].address_components[3].long_name.split(' ')
+    .then( geolocation =>{
+        let county = geolocation.data.results[0].address_components[3].long_name.split(' ')
         county.pop()
         county = county.join(' ')
         axios.get(`https://corona.lmao.ninja/v2/jhucsse/counties/${county}`)
         .then((response) => {
-            res.render('results/results',{
+            console.log(geolocation.data.results[0].geometry.location)
+            res.render('comments/index',{
                 user: req.user ? req.user : null,
                 results: response.data ? response.data : null,
-                state: req.body.state ? req.body.state : null
+                state: req.body.state ? req.body.state : null,
+                geoLat: geolocation.data.results[0].geometry.location.lat ? geolocation.data.results[0].geometry.location.lat : null,
+                geoLng: geolocation.data.results[0].geometry.location.lng ? geolocation.data.results[0].geometry.location.lng : null,
+                comments: null
             } )
         
     })
@@ -26,7 +30,7 @@ function address(req, res){
 }
 
 function index(req, res){
-    res.render('results/results', { user: req.user ? req.user: null, results: null})
+    res.render('comments/index', { user: req.user ? req.user: null, results: null})
 }
 
 function search(req, res) {
